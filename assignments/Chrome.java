c04a9e6b6868bc1b3b0d2f21cf8417bc8e516323
AkshayaK28 <axyakrishnaraj@gmail.com>
2025-09-13 05:43:41 -0700

    Add files via upload

---
+++ assignments/Chrome.java
@@ -0,0 +1,23 @@
+package assignments;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	float version = 91.0f;
+	String developer = "Google";
+	boolean isBeta = false;
+	int releaseYear = 2008;
+	char shortcutKey = 'C';
+	
+	System.out.println("Version is "+version);
+	System.out.println("Developere is "+developer);
+	System.out.println("isbeta is "+isBeta);
+	System.out.println("releaseYear is "+releaseYear);
+	System.out.println("shortcutKey is "+shortcutKey);
+	
+	}
+
+}
